7aa8f6ba6ba43d45fa4480ef23151e001371ae7f
Pranjal Tripathi <64363696+pranjalvkt@users.noreply.github.com>
2020-06-04 18:25:19 +0530

    Add files via upload

---
+++ Day19.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int a) {
+        int num = 1;
+        int temp = 0,sum = 0;
+        while(num <= a){
+            if(a % num == 0) {
+                temp = num;
+                sum += temp;
+            }
+            num++;
+        }
+    return sum;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
